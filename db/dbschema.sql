806595e7aa81f1db608ac708b7b18dc9f6e6694f
jciberta <Josep Ciberta>
2019-09-27 12:09:05 +0200

    No cal copiar la baixa de la matrícula anterior

--- db/dbschema.sql
+++ db/dbschema.sql
@@ -400,24 +400,24 @@ DELIMITER //
 CREATE PROCEDURE CopiaNotesAnteriorMatricula(IN AlumneId INT, MatriculaId INT, IN MatriculaAnteriorId INT)
 BEGIN
     DECLARE _uf_id, _nota1, _nota2, _nota3, _nota4, _nota5, _convocatoria INT;
-    DECLARE _exempt, _convalidat, _junta, _baixa BIT;
+    DECLARE _exempt, _convalidat, _junta BIT;
     DECLARE done INT DEFAULT FALSE;
 
     DROP TABLE IF EXISTS NotesTemp;
     CREATE TEMPORARY TABLE NotesTemp AS (SELECT * FROM NOTES WHERE matricula_id=MatriculaAnteriorId);
 
 	BEGIN
-		DECLARE curNotes CURSOR FOR SELECT uf_id, nota1, nota2, nota3, nota4, nota5, exempt, convalidat, junta, baixa, convocatoria FROM NotesTemp;
+		DECLARE curNotes CURSOR FOR SELECT uf_id, nota1, nota2, nota3, nota4, nota5, exempt, convalidat, junta, convocatoria FROM NotesTemp;
 		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
 		OPEN curNotes;
 
 		read_loop: LOOP
-			FETCH curNotes INTO _uf_id, _nota1, _nota2, _nota3, _nota4, _nota5, _exempt, _convalidat, _junta, _baixa, _convocatoria;
+			FETCH curNotes INTO _uf_id, _nota1, _nota2, _nota3, _nota4, _nota5, _exempt, _convalidat, _junta, _convocatoria;
 			IF done THEN
 				LEAVE read_loop;
 			END IF;
-			UPDATE NOTES SET nota1=_nota1, nota2=_nota2, nota3=_nota3, nota4=_nota4, nota5=_nota5, exempt=_exempt, convalidat=_convalidat, junta=_junta, baixa=_baixa, convocatoria=_convocatoria 
+			UPDATE NOTES SET nota1=_nota1, nota2=_nota2, nota3=_nota3, nota4=_nota4, nota5=_nota5, exempt=_exempt, convalidat=_convalidat, junta=_junta, convocatoria=_convocatoria 
 				WHERE matricula_id=MatriculaId AND uf_id=_uf_id;
 		END LOOP;
 
