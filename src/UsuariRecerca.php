2e367cffa96833bb6599c4af0ff97f4f539d6cdf
jciberta <josep.ciberta@gmail.com>
2019-12-10 20:01:41 +0100

    Llistat pares amb últim login

--- src/UsuariRecerca.php
+++ src/UsuariRecerca.php
@@ -203,6 +203,47 @@ switch ($Accio) {
 		$frm->AfegeixOpcioAJAX('Bloquejat', 'BloquejaUsuari', 'usuari_id', [FormRecerca::ofrCHECK], 'usuari_bloquejat');
 		$frm->EscriuHTML();
         break;
+    case "UltimLogin":
+		$frm = new FormRecerca($conn, $Usuari);
+		$frm->AfegeixJavaScript('Matricula.js?v1.4');
+		$frm->Modalitat = $Modalitat;
+		$frm->Titol = "Últims logins";
+		$Where = ($CursId > 0) ? ' AND C.curs_id='.$CursId : '';
+		
+		$SQL = ' SELECT '.
+			' 	U.usuari_id AS UsuariId, U.nom AS NomAlumne, U.cognom1 AS Cognom1Alumne, U.cognom2 AS Cognom2Alumne, U.username, '.
+			' 	Edat(U.data_naixement) AS edat, FormataData(U.data_ultim_login) AS UltimLoginAlumne, '.
+			' 	UP.username AS NIFPare, UP.nom AS NomPare, UP.cognom1 AS Cognom1Pare, UP.cognom2 AS Cognom2Pare, FormataData(UP.data_ultim_login) AS UltimLoginPare,'.
+			' 	UM.username AS NIFMare, UM.nom AS NomMare, UM.cognom1 AS Cognom1Mare, UM.cognom2 AS Cognom2Mare, FormataData(UM.data_ultim_login) AS UltimLoginMare,'.
+			' 	M.matricula_id, M.grup, '.
+			' 	C.codi, C.curs_id AS CursId, C.nom AS NomCurs, C.nivell, M.baixa '.
+			' FROM USUARI U '.
+			' LEFT JOIN USUARI UP ON (UP.usuari_id=U.pare_id) '.
+			' LEFT JOIN USUARI UM ON (UM.usuari_id=U.mare_id) '.
+			' LEFT JOIN MATRICULA M ON (M.alumne_id=U.usuari_id) '.
+			' LEFT JOIN CURS C ON (C.curs_id=M.curs_id) '.
+			' LEFT JOIN ANY_ACADEMIC AA ON (AA.any_academic_id=C.any_academic_id) '.
+			' WHERE U.es_alumne=1 '.$Where.
+			' AND AA.actual=1 '.
+			' ORDER BY C.nom, C.nivell, U.cognom1, U.cognom2, U.nom ';
+//print "<br><br><br><br><br>SQL: $SQL";
+		$frm->SQL = $SQL;
+		$frm->Taula = 'USUARI';
+		$frm->ClauPrimaria = 'UsuariId';
+		$frm->Camps = 'NomAlumne, Cognom1Alumne, Cognom2Alumne, UltimLoginAlumne, edat, codi, nivell, grup, NomPare, Cognom1Pare, Cognom2Pare, UltimLoginPare, NomMare, Cognom1Mare, Cognom2Mare, UltimLoginMare';
+		$frm->Descripcions = 'Nom, 1r cognom, 2n cognom, Últim login, Edat, Curs, Nivell, Grup, Nom resp1, 1r cognom resp1, 2n cognom resp1, Últim login, Nom resp2, 1r cognom resp2, 2n cognom resp2, Últim login';
+
+		// Filtre
+		if ($CursId < 0) {
+			$aCurs = ObteCodiValorDesDeSQL($conn, "SELECT curs_id, nom FROM CURS", "curs_id", "nom");
+			array_unshift($aCurs[0], '');
+			array_unshift($aCurs[1], '');
+			$frm->Filtre->AfegeixLlista('CursId', 'Curs', 100, $aCurs[0], $aCurs[1]);
+		}
+		$frm->Filtre->AfegeixLlista('grup', 'Grup', 30, array('', 'A', 'B', 'C'), array('', 'A', 'B', 'C'));
+
+		$frm->EscriuHTML();
+        break;
 }
 
 $conn->close();
