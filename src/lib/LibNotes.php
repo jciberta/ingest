f7cfb0ed3586ac98f690351baabc6a604caf910d
jciberta <josep.ciberta@gmail.com>
2019-12-10 14:35:16 +0100

    Correcció notes vàlides

--- src/lib/LibNotes.php
+++ src/lib/LibNotes.php
@@ -21,36 +21,6 @@ require_once(ROOT.'/lib/LibProfessor.php');
 require_once(ROOT.'/lib/LibAvaluacio.php');
 
 /**
- * CreaSQLNotes
- *
- * Crea la sentència SQL per recuperar les notes d'un cicle i un nivell concret.
- *
- * @param string $CicleId Identificador del cicle formatiu.
- * @param string $Nivell Nivell: 1r o 2n.
- * @return string Sentència SQL.
- */
-/*function CreaSQLNotes($CursId, $Nivell)
-//function CreaSQLNotes($CicleId, $Nivell)
-{
-	return ' SELECT M.alumne_id AS AlumneId, '.
-		' U.nom AS NomAlumne, U.cognom1 AS Cognom1Alumne, U.cognom2 AS Cognom2Alumne, '.
-		' UF.unitat_formativa_id AS unitat_formativa_id, UF.codi AS CodiUF, UF.hores AS Hores, UF.orientativa AS Orientativa, UF.nivell AS NivellUF, '.
-		' MP.codi AS CodiMP, '.
-		' N.notes_id AS NotaId, N.baixa AS BaixaUF, N.convocatoria AS Convocatoria, N.convalidat AS Convalidat, '.
-		' M.grup AS Grup, M.grup_tutoria AS GrupTutoria, M.baixa AS BaixaMatricula, C.nivell AS NivellMAT, '.
-		' N.*, U.* '.
-		' FROM NOTES N '.
-		' LEFT JOIN MATRICULA M ON (M.matricula_id=N.matricula_id) '.
-		' LEFT JOIN CURS C ON (C.curs_id=M.curs_id) '.
-		' LEFT JOIN USUARI U ON (M.alumne_id=U.usuari_id) '.
-		' LEFT JOIN UNITAT_FORMATIVA UF ON (UF.unitat_formativa_id=N.uf_id) '.
-		' LEFT JOIN MODUL_PROFESSIONAL MP ON (MP.modul_professional_id=UF.modul_professional_id) '.
-		' WHERE C.curs_id='.$CursId.' OR C.curs_id=2 '.
-//		' WHERE C.cicle_formatiu_id='.$CicleId.' AND C.nivell>='.$Nivell.
-		' ORDER BY C.nivell, U.cognom1, U.cognom2, U.nom, MP.codi, UF.codi ';	
-}*/
- 
-/**
  * ObteTaulaNotesJSON
  *
  * Recupera les notes d'un cicle i un nivell concret en format JSON.
@@ -85,7 +55,8 @@ function ObteTaulaNotesJSON($Connexio, $CicleId, $Nivell)
  * @return boolean Cert si és una valor vàlid com a nota.
  */
 function EsNotaValida($Valor)
-{
+{	
+	$Valor = strtoupper($Valor);
 	return ((is_numeric($Valor) && ($Valor>0) && ($Valor<=10)) ||
 		($Valor == 'NP') ||
 		($Valor == 'A') ||
@@ -106,6 +77,7 @@ function EsNotaValida($Valor)
  */
 function NotaANumero($Valor)
 {
+	$Valor = strtoupper($Valor);
 	if ($Valor == 'NP') 
 		return -1;
 	else if ($Valor == 'A') 
