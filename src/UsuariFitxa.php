69d9c3194494f5d996ed4514ae5bc08aafee113b
jciberta <josep.ciberta@gmail.com>
2020-12-13 15:15:38 +0100

    Reorganització objecte Usuari

--- src/UsuariFitxa.php
+++ src/UsuariFitxa.php
@@ -37,77 +37,14 @@ if (!$Usuari->es_admin && !$Usuari->es_direccio && !$Usuari->es_cap_estudis && !
 if ($Usuari->es_professor && $Id == -1)
 	header("Location: Surt.php");
 
-// Només poden veure la fitxa els tutors d'aquell alumne
+// Només poden veure la fitxa els tutors d'aquell alumne.
 $Professor = new Professor($conn, $Usuari);
 $ProfessorSenseCarrecDirectiu = ($Usuari->es_professor) && (!$Usuari->es_direccio) && (!$Usuari->es_cap_estudis);
 if ($ProfessorSenseCarrecDirectiu && !$Professor->EsTutorAlumne($Id))
 	header("Location: Surt.php");
 
-$frm = new FormFitxa($conn, $Usuari);
-$frm->Titol = 'Fitxa usuari';
-$frm->Taula = 'USUARI';
-$frm->ClauPrimaria = 'usuari_id';
-$frm->Id = $Id;
-$frm->NomesLectura = $ProfessorSenseCarrecDirectiu;
-
-$frm->AfegeixText('username', 'Usuari', 100, [FormFitxa::offREQUERIT]);
-$frm->AfegeixText('nom', 'Nom', 100, [FormFitxa::offREQUERIT]);
-$frm->AfegeixText('cognom1', '1r cognom', 100, [FormFitxa::offREQUERIT]);
-$frm->AfegeixText('cognom2', '2n cognom', 100, [FormFitxa::offAL_COSTAT]);
-
-$frm->Pestanya('Fotografia');
-$frm->AfegeixFotografia('document', '.jpg');
-
-$frm->Pestanya('Dades');
-$frm->AfegeixText('codi', 'Codi (codi professor, IDALU per alumne)', 100);
-$frm->AfegeixLlista('sexe', 'Sexe', 30, array('H', 'D', 'N'), array('Home', 'Dona', 'Neutre'), [FormFitxa::offREQUERIT]);
-$frm->AfegeixLlista('tipus_document', 'Tipus document', 30, array('D', 'N', 'P'), array('Dni', 'Nie', 'Passaport'), [FormFitxa::offREQUERIT]);
-$frm->AfegeixText('document', 'Document', 100, [FormFitxa::offAL_COSTAT]);
-
-$frm->AfegeixData('data_naixement', 'Data naixement');
-$frm->AfegeixCalculat(Form::tccEDAT, 'data_naixement', 'Edat', 100, [FormFitxa::offAL_COSTAT]);
-$frm->AfegeixText('municipi_naixement', 'Municipi naixement', 100);
-$frm->AfegeixText('nacionalitat', 'Nacionalitat', 100);
-$frm->AfegeixText('email', 'Correu electrònic', 100);
-
-//$frm->AfegeixPassword('password', 'Contrasenya', 100, [FormFitxa::offREQUERIT]);
-if (!$Usuari->es_professor) {
-	$frm->AfegeixCheckBox('imposa_canvi_password', 'Imposa nova contrasenya?');
-	$frm->AfegeixCheckBox('usuari_bloquejat', "Bloqueja l'usuari?");
-}
-
-$frm->Pestanya('Contacte');
-$frm->AfegeixText('telefon', 'Telèfons', 100);
-$frm->AfegeixText('adreca', 'Adreça', 100);
-$frm->AfegeixText('codi_postal', 'Codi postal', 100);
-$frm->AfegeixText('poblacio', 'Població', 100);
-$frm->AfegeixText('municipi', 'Municipi', 100);
-$frm->AfegeixText('provincia', 'Província', 100);
-$frm->AfegeixCheckBox('permet_tutor', "Permet tutor? (vàlid pels >=18 anys)");
-
-$frm->Pestanya('Pares');
-$frm->AfegeixLookUp('pare_id', 'Pare', 100, 'UsuariRecerca.php?accio=Pares', 'USUARI', 'usuari_id', 'nom, cognom1, cognom2');
-$frm->AfegeixLookUp('mare_id', 'Mare', 100, 'UsuariRecerca.php?accio=Pares', 'USUARI', 'usuari_id', 'nom, cognom1, cognom2');
-
-if (!$Usuari->es_professor) {
-	$frm->Pestanya('Rols');
-	//$frm->IniciaColumnes();
-	$frm->AfegeixCheckBox('es_direccio', 'És direcció?');
-	$frm->AfegeixCheckBox('es_cap_estudis', "És cap d'estudis?", [FormFitxa::offAL_COSTAT]);
-	$frm->AfegeixCheckBox('es_cap_departament', "És cap de departament?", [FormFitxa::offAL_COSTAT]);
-	//$frm->SaltaColumna();
-	$frm->AfegeixCheckBox('es_tutor', "És tutor?");
-	$frm->AfegeixCheckBox('es_professor', "És professor?", [FormFitxa::offAL_COSTAT]);
-	$frm->AfegeixCheckBox('es_alumne', "És alumne?", [FormFitxa::offAL_COSTAT]);
-	//$frm->SaltaColumna();
-	$frm->AfegeixCheckBox('es_pare', "És pare?");
-	//$frm->FinalitzaColumnes();
-
-}
-$frm->Pestanya('Expedient');
-$Alumne = new Alumne($conn, $Usuari);
-$frm->AfegeixHTML($Alumne->Matricules($Id), 'Matrícules');
-
-$frm->EscriuHTML();
+// Creació del formulari de la fitxa de l'usuari.
+$frm = new Usuari($conn, $Usuari);
+$frm->EscriuFormulariFitxa($Id);
 
 ?>
--- src/lib/LibUsuari.php
+++ src/lib/LibUsuari.php
@@ -9,32 +9,43 @@
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License version 3
  */
 
+require_once(ROOT.'/lib/LibForms.php');
+
 /**
  * Classe que encapsula les utilitats per al maneig de l'usuari.
+ * No es creen les subclasses Professor, Alumne... ja que un usuari pot ser diversos rols a la vegada.
  */
 class Usuari
 {
 	/**
 	* Connexió a la base de dades.
-	* @access public 
 	* @var object
 	*/    
 	public $Connexio;
 
 	/**
 	* Usuari autenticat.
-	* @access public 
 	* @var object
 	*/    
 	public $Usuari;
 
 	/**
+	* Registre de la base de dades que conté les dades d'una matrícula.
+	* @var object
+	*/    
+    private $Registre = null;
+
+	/**
 	 * Constructor de l'objecte.
+	 * El registre queda carregat inicialment amb l'usuari de l'aplicació.
+	 * Si es vol canviar d'usuari, cal usar el mètode Carrega.
 	 * @param objecte $conn Connexió a la base de dades.
+	 * @param object $user Usuari de l'aplicació.
 	 */
 	function __construct($con, $user) {
 		$this->Connexio = $con;
 		$this->Usuari = $user;
+		$this->Registre = $user;
 	}	
 
 	/**
@@ -62,8 +73,16 @@ class Usuari
 	}	
 
 	/**
+	 * Comprova si l'usuari és alumne.
+	 * @returns boolean Cert si l'usuari és alumne.
+	 */
+	function EsAlumne(): bool {
+		return $this->Usuari->es_alumne == '1';
+	}	
+
+	/**
 	 * Comprova si l'usuari és pare/mare d'un alumne.
-	 * @param $alumne Identificador de l'alumne
+	 * @param $alumne Identificador de l'alumne.
 	 * @returns boolean Cert si l'usuari és pare/mare de l'alumne.
 	 */
 	function EsProgenitor(int $alumne): bool {
@@ -71,13 +90,101 @@ class Usuari
 		$ResultSet = $this->Connexio->query($SQL);
 		return ($ResultSet->num_rows > 0);
 	}	
-}
 
-/**
- * Classe que encapsula les utilitats per al maneig dels alumnes.
- */
-class Alumne extends Usuari
-{
+	/**
+	 * Carrega les dades d'un usuari i les emmagatzema en l'atribut Registre.
+     * @param int $UsuariId Identificador de l'usuari.
+	 */
+	public function Carrega(int $UsuariId) {
+		$SQL = " SELECT * FROM USUARI WHERE usuari_id=$UsuariId ";
+		$ResultSet = $this->Connexio->query($SQL);
+		if ($ResultSet->num_rows > 0) {		
+			$rs = $ResultSet->fetch_object();
+			$this->Registre = $rs;
+		}
+	}
+	
+	/**
+	 * Genera i escriu el formulari de la fitxa.
+	 * @param $UsuariId Identificador de l'usuari.
+	 */
+	public function EscriuFormulariFitxa(int $UsuariId) {
+		$frm = new FormFitxa($this->Connexio, $this->Usuari);
+		$frm->Titol = 'Fitxa usuari';
+		$frm->Taula = 'USUARI';
+		$frm->ClauPrimaria = 'usuari_id';
+		$frm->Id = $UsuariId;
+		// Carreguem les dades de l'usuari al registre
+		$this->Carrega($UsuariId);
+		
+		$ProfessorSenseCarrecDirectiu = ($this->Usuari->es_professor) && (!$this->Usuari->es_direccio) && (!$this->Usuari->es_cap_estudis);
+		$frm->NomesLectura = $ProfessorSenseCarrecDirectiu;
+
+		$frm->AfegeixText('username', 'Usuari', 100, [FormFitxa::offREQUERIT]);
+		$frm->AfegeixText('nom', 'Nom', 100, [FormFitxa::offREQUERIT]);
+		$frm->AfegeixText('cognom1', '1r cognom', 100, [FormFitxa::offREQUERIT]);
+		$frm->AfegeixText('cognom2', '2n cognom', 100, [FormFitxa::offAL_COSTAT]);
+
+		if ($this->Registre->es_alumne) {
+			$frm->Pestanya('Fotografia');
+			$frm->AfegeixFotografia('document', '.jpg');
+		}
+
+		$frm->Pestanya('Dades');
+		$frm->AfegeixText('codi', 'Codi (codi professor, IDALU per alumne)', 100);
+		$frm->AfegeixLlista('sexe', 'Sexe', 30, array('H', 'D', 'N'), array('Home', 'Dona', 'Neutre'), [FormFitxa::offREQUERIT]);
+		$frm->AfegeixLlista('tipus_document', 'Tipus document', 30, array('D', 'N', 'P'), array('Dni', 'Nie', 'Passaport'), [FormFitxa::offREQUERIT]);
+		$frm->AfegeixText('document', 'Document', 100, [FormFitxa::offAL_COSTAT]);
+
+		$frm->AfegeixData('data_naixement', 'Data naixement');
+		$frm->AfegeixCalculat(Form::tccEDAT, 'data_naixement', 'Edat', 100, [FormFitxa::offAL_COSTAT]);
+		$frm->AfegeixText('municipi_naixement', 'Municipi naixement', 100);
+		$frm->AfegeixText('nacionalitat', 'Nacionalitat', 100);
+		$frm->AfegeixText('email', 'Correu electrònic', 100);
+
+		//$frm->AfegeixPassword('password', 'Contrasenya', 100, [FormFitxa::offREQUERIT]);
+		if (!$Usuari->es_professor) {
+			$frm->AfegeixCheckBox('imposa_canvi_password', 'Imposa nova contrasenya?');
+			$frm->AfegeixCheckBox('usuari_bloquejat', "Bloqueja l'usuari?");
+		}
+
+		$frm->Pestanya('Contacte');
+		$frm->AfegeixText('telefon', 'Telèfons', 100);
+		$frm->AfegeixText('adreca', 'Adreça', 100);
+		$frm->AfegeixText('codi_postal', 'Codi postal', 100);
+		$frm->AfegeixText('poblacio', 'Població', 100);
+		$frm->AfegeixText('municipi', 'Municipi', 100);
+		$frm->AfegeixText('provincia', 'Província', 100);
+		$frm->AfegeixCheckBox('permet_tutor', "Permet tutor? (vàlid pels >=18 anys)");
+
+		if ($this->Registre->es_alumne) {
+			$frm->Pestanya('Pares');
+			$frm->AfegeixLookUp('pare_id', 'Pare', 100, 'UsuariRecerca.php?accio=Pares', 'USUARI', 'usuari_id', 'nom, cognom1, cognom2');
+			$frm->AfegeixLookUp('mare_id', 'Mare', 100, 'UsuariRecerca.php?accio=Pares', 'USUARI', 'usuari_id', 'nom, cognom1, cognom2');
+		}
+
+		if (!$this->Usuari->es_professor) {
+			$frm->Pestanya('Rols');
+			//$frm->IniciaColumnes();
+			$frm->AfegeixCheckBox('es_direccio', 'És direcció?');
+			$frm->AfegeixCheckBox('es_cap_estudis', "És cap d'estudis?", [FormFitxa::offAL_COSTAT]);
+			$frm->AfegeixCheckBox('es_cap_departament', "És cap de departament?", [FormFitxa::offAL_COSTAT]);
+			//$frm->SaltaColumna();
+			$frm->AfegeixCheckBox('es_tutor', "És tutor?");
+			$frm->AfegeixCheckBox('es_professor', "És professor?", [FormFitxa::offAL_COSTAT]);
+			$frm->AfegeixCheckBox('es_alumne', "És alumne?", [FormFitxa::offAL_COSTAT]);
+			//$frm->SaltaColumna();
+			$frm->AfegeixCheckBox('es_pare', "És pare?");
+			//$frm->FinalitzaColumnes();
+
+		}
+		if ($this->Registre->es_alumne) {
+			$frm->Pestanya('Expedient');
+			$frm->AfegeixHTML($this->Matricules($UsuariId), 'Matrícules');
+		}
+		$frm->EscriuHTML();
+	}
+	
 	/**
 	 * Obté l'identificador de la matrícula activa d'un alumne.
 	 * @param $AlumneId Identificador de l'alumne
@@ -123,6 +230,14 @@ class Alumne extends Usuari
 		}		
 		return $Retorn;
 	}	
+}
+
+/**
+ * Classe que encapsula les utilitats per al maneig dels alumnes.
+ */
+class Alumne extends Usuari
+{
+	
 }
 
 ?>
