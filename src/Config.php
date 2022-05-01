93531c7fb9838ccd4516533201c9aebf3b894245
Josep Ciberta <josep.ciberta@gmail.com>
2022-05-01 19:05:00 +0200

    Versió 1.5

--- src/Config.php
+++ src/Config.php
@@ -10,7 +10,7 @@
  */
 
 class Config {
-	const Versio         = '1.4';
+	const Versio         = '1.5';
 	const Host           = 'localhost';
 	const BaseDades      = 'InGest';
 	const Usuari         = 'root';
