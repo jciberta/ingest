30ac4343eac38f67217e978ee0497764621e8d9e
jciberta <josep.ciberta@gmail.com>
2022-05-10 07:39:42 +0200

    Versió 1.6

--- src/Config.php
+++ src/Config.php
@@ -10,7 +10,7 @@
  */
 
 class Config {
-	const Versio         = '1.5';
+	const Versio         = '1.6';
 	const Host           = 'localhost';
 	const BaseDades      = 'InGest';
 	const Usuari         = 'root';
