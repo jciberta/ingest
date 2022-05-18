9f0b9a11cab8b739c8d2bc3c420e4aaea6af0e7e
jciberta <josep.ciberta@gmail.com>
2022-05-18 17:20:19 +0200

    Formatació de les taules a les programacions

--- src/lib/LibForms.php
+++ src/lib/LibForms.php
@@ -2369,7 +2369,16 @@ class FormFitxa extends Form
 		// Iniciem el supressor de XSS
 		$config = HTMLPurifier_Config::createDefault();
 		// Configuracions http://htmlpurifier.org/demo.php
-		$config->set('HTML.Allowed', 'a[href|title],img[title|src|alt],em,strong,cite,blockquote,code,ul,ol,li,dl,dt,dd,p,br,h1,h2,h3,h4,h5,h6,span,font[color],*[style]');
+		// *[style] means that you allow for all the accepted tags the attribute style
+		$config->set(
+			'HTML.Allowed', 
+			'a[href|title|target],img[title|src|alt],em,strong,cite,blockquote,code,
+			 ul,ol,li,dl,dt,dd,
+			 p,br,
+			 h1,h2,h3,h4,h5,h6,
+			 table[class],tr[align],th[align],td[align],
+			 span,font[color],*[style]
+			');
 		$purifier = new HTMLPurifier($config);
 		
 		$Retorn = '';
