1f6ef0e08457bfc5cb4be0d3172995701d7a8a3b
Josep Ciberta <josep.ciberta@gmail.com>
2022-12-13 14:48:00 +0100

    Actualització correccions

---
+++ util/CorreccionsUTF.sql
@@ -0,0 +1,43 @@
+
+/* Correcció de text UTF */
+
+UPDATE MODUL_PLA_ESTUDI SET metodologia = REPLACE(metodologia, 'Ã¨', 'è');
+UPDATE MODUL_PLA_ESTUDI SET metodologia = REPLACE(metodologia, 'Ã©', 'é');
+UPDATE MODUL_PLA_ESTUDI SET metodologia = REPLACE(metodologia, 'Ã²', 'ò');
+UPDATE MODUL_PLA_ESTUDI SET metodologia = REPLACE(metodologia, 'Ã³', 'ó');
+UPDATE MODUL_PLA_ESTUDI SET metodologia = REPLACE(metodologia, 'â€™', '´');
+UPDATE MODUL_PLA_ESTUDI SET metodologia = REPLACE(metodologia, 'Ã§', 'ç');
+UPDATE MODUL_PLA_ESTUDI SET metodologia = REPLACE(metodologia, 'Ã­', 'í');
+UPDATE MODUL_PLA_ESTUDI SET metodologia = REPLACE(metodologia, 'Ã¯', 'ï');
+UPDATE MODUL_PLA_ESTUDI SET metodologia = REPLACE(metodologia, 'Ãº', 'ú');
+
+UPDATE MODUL_PLA_ESTUDI SET criteris_avaluacio = REPLACE(criteris_avaluacio, 'Ã¨', 'è');
+UPDATE MODUL_PLA_ESTUDI SET criteris_avaluacio = REPLACE(criteris_avaluacio, 'Ã©', 'é');
+UPDATE MODUL_PLA_ESTUDI SET criteris_avaluacio = REPLACE(criteris_avaluacio, 'Ã²', 'ò');
+UPDATE MODUL_PLA_ESTUDI SET criteris_avaluacio = REPLACE(criteris_avaluacio, 'Ã³', 'ó');
+UPDATE MODUL_PLA_ESTUDI SET criteris_avaluacio = REPLACE(criteris_avaluacio, 'â€™', '´');
+UPDATE MODUL_PLA_ESTUDI SET criteris_avaluacio = REPLACE(criteris_avaluacio, 'Ã§', 'ç');
+UPDATE MODUL_PLA_ESTUDI SET criteris_avaluacio = REPLACE(criteris_avaluacio, 'Ã­', 'í');
+UPDATE MODUL_PLA_ESTUDI SET criteris_avaluacio = REPLACE(criteris_avaluacio, 'Ã¯', 'ï');
+UPDATE MODUL_PLA_ESTUDI SET criteris_avaluacio = REPLACE(criteris_avaluacio, 'Ãº', 'ú');
+
+UPDATE MODUL_PLA_ESTUDI SET recursos = REPLACE(recursos, 'Ã¨', 'è');
+UPDATE MODUL_PLA_ESTUDI SET recursos = REPLACE(recursos, 'Ã©', 'é');
+UPDATE MODUL_PLA_ESTUDI SET recursos = REPLACE(recursos, 'Ã²', 'ò');
+UPDATE MODUL_PLA_ESTUDI SET recursos = REPLACE(recursos, 'Ã³', 'ó');
+UPDATE MODUL_PLA_ESTUDI SET recursos = REPLACE(recursos, 'â€™', '´');
+UPDATE MODUL_PLA_ESTUDI SET recursos = REPLACE(recursos, 'Ã§', 'ç');
+UPDATE MODUL_PLA_ESTUDI SET recursos = REPLACE(recursos, 'Ã­', 'í');
+UPDATE MODUL_PLA_ESTUDI SET recursos = REPLACE(recursos, 'Ã¯', 'ï');
+UPDATE MODUL_PLA_ESTUDI SET recursos = REPLACE(recursos, 'Ãº', 'ú');
+
+UPDATE MODUL_PLA_ESTUDI SET planificacio = REPLACE(planificacio, 'Ã¨', 'è');
+UPDATE MODUL_PLA_ESTUDI SET planificacio = REPLACE(planificacio, 'Ã©', 'é');
+UPDATE MODUL_PLA_ESTUDI SET planificacio = REPLACE(planificacio, 'Ã²', 'ò');
+UPDATE MODUL_PLA_ESTUDI SET planificacio = REPLACE(planificacio, 'Ã³', 'ó');
+UPDATE MODUL_PLA_ESTUDI SET planificacio = REPLACE(planificacio, 'â€™', '´');
+UPDATE MODUL_PLA_ESTUDI SET planificacio = REPLACE(planificacio, 'Ã§', 'ç');
+UPDATE MODUL_PLA_ESTUDI SET planificacio = REPLACE(planificacio, 'Ã­', 'í');
+UPDATE MODUL_PLA_ESTUDI SET planificacio = REPLACE(planificacio, 'Ã¯', 'ï');
+UPDATE MODUL_PLA_ESTUDI SET planificacio = REPLACE(planificacio, 'Ãº', 'ú');
+
