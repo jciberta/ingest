43f938d77e2e9cbdd185fe7483dc5af5ca50e350
ShAD0W20 <43751763+ShAd0W20@users.noreply.github.com>
2023-02-14 13:43:51 +0100

    :sparkles: Borsa treball apartat públic
    - S'ha actualitzat un camp de la base de dates on hi havia un error ortogràfic
    - Afegit la possibilitat de veure les ofertes de la borsa ja publicades
    - Opció de filtrar ofertes per nom de cicle, empresa, contacte i població
    - Possibilitat d'afegir noves ofertes mitjançant un formulari

---
+++ src/BorsaTreball.php
@@ -0,0 +1,203 @@
+<?php
+
+/**
+ * BorsaTreball.php
+ * 
+ * Pàgina principal de la borsa de treball
+ * 
+ * @author: shad0wstv
+ * @version: 1.0
+ * @since: 1.13
+ * @license: https://opensource.org/licenses/GPL-3.0 GNU General Public License version 3
+ */
+require_once('Config.php');
+require_once('lib/LibBorsaTreball.php');
+
+$BorsaTreball = new BorsaTreball();
+
+echo $BorsaTreball->creaCapcelera();
+?>
+
+<div class="container-fluid mb-4">
+  <div class="m-4 d-inline">
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="w-100 mw-50">
+          <form action="#" method="post" class="cerca">
+            <div class="input-group">
+              <input class="form-control" type="search" name="itemName" placeholder="Nom, cicle, empresa..." aria-label="Search">
+              <span class="input-group-text bg-transparent border-start-0">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="col-sm-8 mt-sm-0 mt-3">
+        <button type="button" class="btn btn-outline-primary float-end" data-bs-toggle="modal" data-bs-target="#modalNovaOferta" onclick="carregaCicles()">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+          </svg>
+          Nova oferta
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid">
+  <div class="rounded-2 table-responsive">
+    <table id="taulaCicles" class="table table-bordered table-striped table-hover">
+      <caption>
+        <div class="d-flex justify-content-center" id="loading">
+          <div class="spinner-grow" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      </caption>
+      <thead class="table-dark">
+        <tr>
+          <th>Empresa</th>
+          <th>Cicle</th>
+          <th>Població</th>
+          <th>Data publicació</th>
+          <th>Accions</th>
+        </tr>
+      </thead>
+      <tbody id="llista-ofertes"></tbody>
+    </table>
+  </div>
+</div>
+
+<!-- Modal detall oferta -->
+<div class="modal fade" id="modalOferta" tabindex="-1" aria-labelledby="modalOfertaLabel">
+  <div class="modal-dialog modal-dialog-scrollable modal-lg modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalOfertaLabel">Oferta</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="spinner-border text-primary text-center" role="status" id="modalLoading">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="container">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title" id="modalOfertaEmpresa"></h4>
+              <h6 class="card-subtitle mb-2 text-muted">
+                <span id="modalOfertaPoblacio"></span>
+                -
+                <span id="modalOfertaCicle"></span>
+              </h6>
+              <div class="card-text">
+                <h5>Contacte</h5>
+                <p>
+                  <span id="modalOfertaTelefon"></span>
+                  -
+                  <span id="modalOfertaEmail"></span>
+                </p>
+              </div>
+              <div class="mb-2 form-group">
+                <h5>Descripció</h5>
+                <textarea class="form-control" id="modalOfertaDescripcio" rows="10" readonly></textarea>
+              </div>
+              <a href="#" class="btn btn-primary" target="_blank" id="modalOfertaWeb">Visitar web</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+          </svg>
+          Tancar
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal nova oferta -->
+<div class="modal fade" id="modalNovaOferta" tabindex="-1" role="dialog" aria-labelledby="modalNovaOfertaLabel">
+  <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+    <div class=" modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalNovaOfertaLabel">Nova oferta</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="container-fluid">
+          <div class="alert alert-danger fade show visually-hidden" role="alert" id="modalError">
+            <span id="modalErrorMessage"></span>
+          </div>
+          <form class="row g-3">
+            <div class="form-group">
+              <label class="form-label" for="inputCicle">Cicle</label>
+              <select id="inputCicle" class="form-select">
+                <option selected hidden>Escull...</option>
+              </select>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="form-label" for="inputEmpresa">Empresa</label>
+                <input type="text" class="form-control" id="inputEmpresa">
+              </div>
+              <div class="col-md-6">
+                <label class="form-label" for="inputContacte">Contacte</label>
+                <input type="text" class="form-control" id="inputContacte">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="form-label" for="inputTelefon">Telèfon</label>
+                <input type="tel" class="form-control" id="inputTelefon" pattern="[0-9]{9}">
+              </div>
+              <div class="col-md-6">
+                <label class="form-label" for="inputPoblacio">Població</label>
+                <input type="text" class="form-control" id="inputPoblacio">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="form-label" for="inputCorreu">Correu</label>
+                <input type="email" class="form-control" id="inputCorreu">
+              </div>
+              <div class="col-md-6">
+                <label class="form-label" for="inputWeb">Web</label>
+                <input type="text" class="form-control" id="inputWeb">
+              </div>
+            </div>
+            <div>
+              <label class="form-label" for="inputDescripcio">Descripció</label>
+              <textarea class="form-control" id="inputDescripcio" rows="3"></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="cancelaNovaOferta()">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+          </svg>
+          Tancar
+        </button>
+        <button type="button" class="btn btn-primary" id="guardarOferta" onclick="guardarNovaOferta()">
+          <span class="spinner-border spinner-border-sm visually-hidden" role="status" id="modalLoading" aria-hidden="true"></span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+          </svg>
+          <span id="guardarOfertaText">Guardar</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+echo $BorsaTreball->creaFooter();
+?>
---
+++ src/lib/LibBorsaTreball.php
@@ -0,0 +1,316 @@
+<?php
+
+/**
+ * Classe per gestionar la borsa de treball
+ * 
+ * 
+ * @author  shad0wstv
+ * @version 1.0
+ * @since   1.13
+ * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License version 3
+ */
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once(ROOT . '/vendor/autoload.php');
+require_once(ROOT . '/vendor/PHPMailer/PHPMailer.php');
+require_once(ROOT . '/vendor/PHPMailer/Exception.php');
+require_once(ROOT . '/vendor/PHPMailer/SMTP.php');
+
+require_once(ROOT . '/lib/LibForms.php');
+
+
+class BorsaTreball extends Objecte
+{
+
+  /**
+   * Constructor de la classe
+   * @param mysqli $conn Connexió a la base de dades
+   * @param Usuari $usuari Usuari que ha fet login
+   * @param Sistema $sistema Objecte amb la informació del sistema
+   * @param Professor $professorGestor Objecte amb la informació del professor gestor
+   */
+  function __construct($conn = null, $usuari = null, $sistema = null)
+  {
+    parent::__construct($conn, $usuari, $sistema);
+  }
+
+  /**
+   * Retorna la informació de totes les famílies de formació professional
+   * @return string HTML amb la informació de les famílies
+   */
+  public function consultaCiclesFormatius(): string
+  {
+    $stmt = $this->Connexio->prepare("SELECT cf.nom, cf.cicle_formatiu_id FROM cicle_formatiu cf INNER JOIN familia_fp fp ON cf.familia_fp_id = fp.familia_fp_id;");
+
+    $stmt->execute();
+
+    $resultSet = $stmt->get_result();
+
+    $stmt->close();
+
+    $output = "<option selected hidden>Escull...</option>";
+
+    while ($row = $resultSet->fetch_assoc()) {
+      $output .= "<option value='$row[cicle_formatiu_id]'>$row[nom]</option>";
+    }
+
+    return $output;
+  }
+
+  /**
+   * Retorna la informació de totes les ofertes de la borsa de treball
+   * @return string HTML amb la informació de les ofertes
+   */
+  public function consultaOfertes(): string
+  {
+    $stmt = $this->Connexio->prepare("SELECT bt.*, cf.nom AS nom_cicle, fp.nom AS nom_familia FROM borsa_treball bt INNER JOIN cicle_formatiu cf ON bt.cicle_formatiu_id = cf.cicle_formatiu_id INNER JOIN familia_fp fp ON cf.familia_fp_id = fp.familia_fp_id;");
+
+    $stmt->execute();
+
+    $resultSet = $stmt->get_result();
+
+    $stmt->close();
+    $output = "";
+
+    while ($row = $resultSet->fetch_assoc()) {
+      if ($row["publicat"] === 1) {
+        $data = date_format(date_create($row["data_creacio"]), 'd/m/Y H:i:s');
+        $output .= "
+        <tr>
+          <td>$row[empresa]</td>
+          <td>$row[nom_cicle]</td>
+          <td>$row[poblacio]</td>
+          <td>$data</td>
+          <td>
+            <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modalOferta' onclick='mostraOferta($row[borsa_treball_id])'>
+              <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='align-self-start' viewBox='0 0 16 16'>
+                <path d='M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z'></path>
+                <path d='M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z'></path>
+              </svg>
+              Visualitzar
+            </button>
+          </td>
+        </tr>";
+      }
+    }
+
+    return $output;
+  }
+
+  /**
+   * Retorna la informació en base a la cerca
+   * @param string $filtre Text a cercar
+   * @return string HTML amb la informació de les ofertes
+   */
+
+  public function filtrarOfertes($filtre): string
+  {
+    $stmt = $this->Connexio->prepare("SELECT bt.*, cf.nom AS nom_cicle, fp.nom AS nom_familia FROM borsa_treball bt INNER JOIN cicle_formatiu cf ON bt.cicle_formatiu_id = cf.cicle_formatiu_id INNER JOIN familia_fp fp ON cf.familia_fp_id = fp.familia_fp_id WHERE fp.nom LIKE ? OR cf.nom LIKE ? OR bt.empresa LIKE ? OR bt.contacte LIKE ? OR bt.poblacio LIKE ?;");
+
+    $filtre = "%" . $filtre . "%";
+
+    $stmt->bind_param("sssss", $filtre, $filtre, $filtre, $filtre, $filtre);
+
+    $stmt->execute();
+
+    $resultSet = $stmt->get_result();
+
+    $stmt->close();
+    $output = "";
+
+    while ($row = $resultSet->fetch_assoc()) {
+      if ($row["publicat"] === 1) {
+        $data = date_format(date_create($row["data_creacio"]), 'd/m/Y H:i:s');
+        $output .= "
+        <tr>
+          <td>$row[empresa]</td>
+          <td>$row[nom_cicle]</td>
+          <td>$row[poblacio]</td>
+          <td>$data</td>
+          <td>
+            <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modalOferta' onclick='mostraOferta($row[borsa_treball_id])'>
+              <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='align-self-start' viewBox='0 0 16 16'>
+                <path d='M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z'></path>
+                <path d='M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z'></path>
+              </svg>
+              Visualitzar
+            </button>
+          </td>
+        </tr>";
+      }
+    }
+
+    return $output;
+  }
+
+  /**
+   * Retorna la informació d'una oferta de la borsa de treball a partir del seu id
+   * @param int $id
+   * @return string JSON amb la informació de l'oferta
+   */
+  public function consultaOferta(int $id): string
+  {
+    $stmt = $this->Connexio->prepare("SELECT bt.*, cf.nom AS nom_cicle, fp.nom AS nom_familia FROM borsa_treball bt INNER JOIN cicle_formatiu cf ON bt.cicle_formatiu_id = cf.cicle_formatiu_id INNER JOIN familia_fp fp ON cf.familia_fp_id = fp.familia_fp_id WHERE borsa_treball_id = ?;");
+
+    $stmt->bind_param("i", $id);
+
+    $stmt->execute();
+
+    $resultSet = $stmt->get_result();
+    $resultSet = $resultSet->fetch_all(MYSQLI_ASSOC);
+
+    $stmt->close();
+
+    return json_encode($resultSet);
+  }
+
+  /**
+   * Guarda una nova oferta de la borsa de treball
+   * @param string $empresa
+   * @param int $cicle
+   * @param string $contacte
+   * @param string $telefon
+   * @param string $poblacio
+   * @param string $correu
+   * @param string $descripcio
+   * @param string $web
+   * @return string JSON amb el resultat de la operació
+   */
+  public function guardarNovaOferta($empresa, $cicle, $contacte, $telefon, $poblacio, $correu, $descripcio, $web)
+  {
+    try {
+
+      $stmt = $this->Connexio->prepare("INSERT INTO borsa_treball (cicle_formatiu_id, empresa, contacte, telefon, poblacio, email, web, descripcio) VALUES (?, ?, ?, ?, ?, ?, ?, ?);");
+
+      $stmt->bind_param("isssssss", $cicle, $empresa, $contacte, $telefon, $poblacio, $correu, $web, $descripcio);
+
+      $stmt->execute();
+
+      $stmt->close();
+
+      $this->enviarMailNovaOferta($empresa, $cicle, $contacte, $telefon, $poblacio, $correu, $descripcio, $web);
+    } catch (Exception $e) {
+      if (Config::Debug) {
+        return json_encode(array("status" => "error", "message" => $e->getMessage(), "trace" => $e->getTrace()));
+      }
+
+      return json_encode(array("status" => "error", "message" => "Error al enviar el correu"));
+    }
+
+    return json_encode(array("status" => "ok"));
+  }
+
+  /**
+   * Crea la capçalera de la pàgina
+   * @param string $Titol
+   * @return string HTML amb la capçalera
+   */
+  public function creaCapcelera($Titol = 'Borsa de treball'): string
+  {
+    $Retorn = '<!doctype html>' . PHP_EOL;
+    $Retorn .= '<html>' . PHP_EOL;
+    $Retorn .= '<head>' . PHP_EOL;
+    $Retorn .= '  <meta name="viewport" content="width=device-width, initial-scale=1">' . PHP_EOL;
+    $Retorn .= '	<meta charset=UTF8>' . PHP_EOL;
+    $Retorn .= '	<title>InGest</title>' . PHP_EOL;
+    $Retorn .= '	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">' . PHP_EOL;
+    $Retorn .= '</head>' . PHP_EOL;
+    $Retorn .= '<body>' . PHP_EOL;
+    $Retorn .= '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                  <div class="container-fluid">
+                    <a class="navbar-brand" href="#">InGest</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="/">Inici</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="/BorsaTreball.php">Borsa Treball</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </nav>';
+    $Retorn .= '<div class="starter-template" style="padding:20px">';
+    $Retorn .= '<h1>' . $Titol . '</h1>';
+    return $Retorn;
+  }
+
+  public function creaFooter()
+  {
+    echo "</div>" . PHP_EOL;
+    echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>' . PHP_EOL;
+    echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>' . PHP_EOL;
+    echo '<script src="js/BorsaTreball.js"></script>' . PHP_EOL;
+    echo '</body>' . PHP_EOL;
+    echo '</html>' . PHP_EOL;
+  }
+
+  /**
+   * Envia un correu amb la informació de la nova oferta de treball
+   * @param string $empresa
+   * @param int $cicle
+   * @param string $contacte
+   * @param string $telefon
+   * @param string $poblacio
+   * @param string $correu
+   * @param string $descripcio
+   * @param string $web
+   * @throws Exception Si hi ha algun error al enviar el correu
+   */
+  private function enviarMailNovaOferta($empresa, $cicle, $contacte, $telefon, $poblacio, $correu, $descripcio, $web)
+  {
+
+    $stmt = $this->Connexio->prepare("SELECT u.email FROM usuari u INNER JOIN sistema s ON u.usuari_id = s.gestor_borsa_treball_id;");
+
+    $stmt->execute();
+
+    $rs = $stmt->get_result();
+
+    $stmt->close();
+
+    $body = "
+      <h1>Nova oferta de treball</h1>
+      <p>Empresa: $empresa</p>
+      <p>Cicle: $cicle</p>
+      <p>Contacte: $contacte</p>
+      <p>Telefon: $telefon</p>
+      <p>Població: $poblacio</p>
+      <p>Correu: $correu</p>
+      <p>Descripció: $descripcio</p>
+      <p>Web: $web</p>
+    ";
+
+    $mail = new PHPMailer(true);
+    $mail->SMTPDebug = 0;
+
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 465;
+
+    $mail->IsSMTP();
+    $mail->SMTPSecure = 'ssl';
+    $mail->SMTPAuth = true;
+
+    $mail->Username = Config::Correu;
+    $mail->Password = Config::PasswordCorreu;
+
+    $mail->IsHTML(true);
+    $mail->setFrom(Config::Correu, "No contesteu");
+
+    $mail->Sender = Config::Correu;
+    $mail->Subject = "Nova oferta de treball";
+
+    $mail->isHTML(true);
+    $mail->Body = $body;
+    $mail->AddAddress($rs->fetch_assoc()['email']);
+
+    if (!$mail->Send()) {
+      throw new Exception("Error al enviar el correu");
+    }
+  }
+}
