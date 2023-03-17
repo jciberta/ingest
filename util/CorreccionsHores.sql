6732cc1d3e1fbd0ef28daa99ab6781d8df58b1ec
Josep Ciberta <josep.ciberta@gmail.com>
2023-03-17 19:44:46 +0100

    Correcció hores setmanals mòdul

--- util/CorreccionsHores.sql
+++ util/CorreccionsHores.sql
@@ -17,3 +17,12 @@ GROUP BY UPE.modul_pla_estudi_id;
 UPDATE MODUL_PROFESSIONAL MP SET MP.hores=(SELECT SUM(UF.hores) FROM UNITAT_FORMATIVA UF WHERE UF.modul_professional_id=MP.modul_professional_id);
 
 UPDATE MODUL_PLA_ESTUDI MPE SET MPE.hores=IFNULL((SELECT SUM(UPE.hores) FROM UNITAT_PLA_ESTUDI UPE WHERE UPE.modul_pla_estudi_id=MPE.modul_pla_estudi_id), 0);
+
+
+/* Correcció hores setmanals mòdul */
+
+UPDATE MODUL_PROFESSIONAL SET hores_setmana=hores/33 WHERE es_fct=0 OR es_fct IS NULL;
+UPDATE MODUL_PROFESSIONAL SET hores_setmana=NULL WHERE nom LIKE '%centres de treball%';
+
+UPDATE MODUL_PLA_ESTUDI SET hores_setmana=hores/33 WHERE es_fct=0 OR es_fct IS NULL;
+UPDATE MODUL_PLA_ESTUDI SET hores_setmana=NULL WHERE nom LIKE '%centres de treball%';
