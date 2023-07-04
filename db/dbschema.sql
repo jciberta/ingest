d60acba1b8f731ebe06e5f0ebbdf354e7a70b89e
Josep Ciberta <josep.ciberta@gmail.com>
2023-07-04 17:11:56 +0200

    Eines per a la homogeneització de les programacions

--- db/dbschema.sql
+++ db/dbschema.sql
@@ -1122,6 +1122,31 @@ END //
 DELIMITER ;
 
 /*
+ * SuprimeixPropietatCSS
+ * Suprimeix la propietat CSS especificada del camp d'una taula.
+ * Ús: CALL SuprimeixPropietatCSS('MODUL_PLA_ESTUDI', 'metodologia', 'font-family:');
+ * @param string Taula.
+ * @param string Camp.
+ * @param string Propietat.
+ */
+DELIMITER //
+CREATE PROCEDURE SuprimeixPropietatCSS
+(
+    IN Taula VARCHAR(50), 
+    IN Camp VARCHAR(50), 
+    IN Propietat VARCHAR(50) 
+)
+BEGIN
+    SET @SQL = CONCAT("UPDATE ", Taula, " SET ", Camp, "=");
+    SET @LocatePropietat = CONCAT("locate('", Propietat, "', ", Camp, ")");
+    SET @SubStr = CONCAT("substr(", Camp, ", ", @LocatePropietat, ", locate(';', ", Camp, ", ", @LocatePropietat, ")-", @LocatePropietat, "+1)"); 
+    SET @SQL = CONCAT(@SQL, "replace(", Camp, ", ", @SubStr, ", '')");
+    PREPARE stmt FROM @SQL;
+    EXECUTE stmt;
+END //
+DELIMITER ;
+
+/*
  * FormataData
  *
  * Donat un camp de tipus data, el retorna en el format dd/mm/yyyy.
--- db/dbupdate_v1.14.sql
+++ db/dbupdate_v1.14.sql
@@ -1,6 +1,31 @@
 /*
-Actualització de la DB a partir de la versió 1.14
-*/
+ * Actualització de la DB a partir de la versió 1.14
+ */
+
+/*
+ * SuprimeixPropietatCSS
+ * Suprimeix la propietat CSS especificada del camp d'una taula.
+ * Ús: CALL SuprimeixPropietatCSS('MODUL_PLA_ESTUDI', 'metodologia', 'font-family:');
+ * @param string Taula.
+ * @param string Camp.
+ * @param string Propietat.
+ */
+DELIMITER //
+CREATE PROCEDURE SuprimeixPropietatCSS
+(
+    IN Taula VARCHAR(50), 
+    IN Camp VARCHAR(50), 
+    IN Propietat VARCHAR(50) 
+)
+BEGIN
+    SET @SQL = CONCAT("UPDATE ", Taula, " SET ", Camp, "=");
+    SET @LocatePropietat = CONCAT("locate('", Propietat, "', ", Camp, ")");
+    SET @SubStr = CONCAT("substr(", Camp, ", ", @LocatePropietat, ", locate(';', ", Camp, ", ", @LocatePropietat, ")-", @LocatePropietat, "+1)"); 
+    SET @SQL = CONCAT(@SQL, "replace(", Camp, ", ", @SubStr, ", '')");
+    PREPARE stmt FROM @SQL;
+    EXECUTE stmt;
+END //
+DELIMITER ;
 
 /* Aplicar a producció */
 UPDATE SISTEMA SET versio_db='1.15';
