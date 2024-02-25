5349995a213aa17030558526f9d524781af0acbf
Josep Ciberta <josep.ciberta@gmail.com>
2024-02-25 17:23:28 +0100

    Procediment emmagatzemat del càlcul de la nota mitjana del curs

--- db/dbschema.sql
+++ db/dbschema.sql
@@ -877,6 +877,132 @@ DELIMITER ;
 
 
 /*
+ * CalculaNotaMitjanaModul
+ * Calcula la nota mitjana d'un mòdul per a una matrícula. Si la mitjana ja ha estat calculada, no es toca.
+ * Càlcul de la nota mitjana de mòdul:
+ *   1. Si hi ha alguna UF que no té nota, no es calcula.
+ *   2. Si totes les UF estan aprovades, es fa la mitja ponderada amb les hores.
+ *   3. Si totes les UF tenen notes i hi ha alguna suspesa, es fa la mitja ponderada amb les hores, i si aquesta és superior a 4, es queda un 4.
+ * @param integer MatriculaId Identificador de la matrícula.
+ * @param integer ModulPlaEstudiId Identificador del mòdul.
+ */
+DELIMITER //
+CREATE PROCEDURE CalculaNotaMitjanaModul(IN MatriculaId INT, IN ModulPlaEstudiId INT)
+BEGIN
+    DECLARE HiHaNotaNulla INT DEFAULT 0;
+    DECLARE HiHaNotaSuspesa INT DEFAULT 0;
+    DECLARE NotaMitjana REAL DEFAULT 0;
+    DECLARE NotaMitjanaEntera INT DEFAULT 0;
+    DECLARE NotaMP INT DEFAULT 0;
+    DECLARE HoresTotals INT DEFAULT 0;
+    DECLARE _NotaConvocatoria INT;
+    DECLARE _hores INT;
+    DECLARE done INT DEFAULT FALSE;
+
+    BEGIN
+        /* Càlcul de la nota mitjana de mòdul */
+        DECLARE cur CURSOR FOR
+            SELECT ObteNotaConvocatoriaI0(nota1, nota2, nota3, nota4, nota5, convocatoria) AS NotaConvocatoria, UPE.hores
+            FROM NOTES N
+            LEFT JOIN UNITAT_PLA_ESTUDI UPE ON (UPE.unitat_pla_estudi_id=N.uf_id)
+            LEFT JOIN MODUL_PLA_ESTUDI MPE ON (MPE.modul_pla_estudi_id=UPE.modul_pla_estudi_id)
+            WHERE N.matricula_id=MatriculaId AND MPE.modul_pla_estudi_id=ModulPlaEstudiId;        
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+        OPEN cur;
+        read_loop: LOOP
+            FETCH cur INTO _NotaConvocatoria, _hores;
+            IF done THEN
+                LEAVE read_loop;
+            END IF;
+            SET HoresTotals = HoresTotals + _hores;
+            IF _NotaConvocatoria IS NOT NULL THEN
+                SET NotaMitjana = NotaMitjana + CONVERT(_NotaConvocatoria*_hores, FLOAT);
+                IF _NotaConvocatoria<5 THEN
+                    SET HiHaNotaSuspesa = 1;
+                END IF;
+            ELSE
+                SET HiHaNotaNulla = 1;
+            END IF;
+        END LOOP;
+        CLOSE cur;
+        SET NotaMitjana = NotaMitjana / CONVERT(HoresTotals, FLOAT);
+        SET NotaMitjanaEntera = ROUND(NotaMitjana);
+        IF HiHaNotaSuspesa=1 THEN
+            SET NotaMitjanaEntera = LEAST(NotaMitjanaEntera, 4);
+        END IF;
+        /* Actualitzem la nota si no és nul·la (NotaMitjanaEntera<>NULL) i a la taula NOTES_MP no hi ha posada una nota */
+        SET NotaMP = (SELECT NMP.nota FROM NOTES_MP NMP
+            WHERE NMP.matricula_id=MatriculaId AND modul_professional_id=(SELECT modul_professional_id FROM MODUL_PLA_ESTUDI WHERE modul_pla_estudi_id=ModulPlaEstudiId)
+        );
+        IF NotaMP IS NULL AND HiHaNotaNulla<>1 THEN
+            INSERT INTO NOTES_MP (matricula_id, modul_professional_id, nota) VALUES (MatriculaId, ModulPlaEstudiId, NotaMitjanaEntera);
+        END IF;
+    END;
+END //
+DELIMITER ;
+
+/*
+ * CalculaNotaMitjanaMatricula
+ * Calcula les notes mitjanes d'una matrícula. Ho fa mòdul a mòdul.
+ * @param integer IdMatricula Identificador de la matrícula.
+ */
+DELIMITER //
+CREATE PROCEDURE CalculaNotaMitjanaMatricula(IN IdMatricula INT)
+BEGIN
+    DECLARE _ModulId INT;
+    DECLARE done INT DEFAULT FALSE;
+
+    BEGIN
+        DECLARE cur CURSOR FOR
+		    SELECT DISTINCT(MPE.modul_pla_estudi_id)
+		    FROM NOTES N
+		    LEFT JOIN UNITAT_PLA_ESTUDI UPE ON (UPE.unitat_pla_estudi_id=N.uf_id)
+		    LEFT JOIN MODUL_PLA_ESTUDI MPE ON (MPE.modul_pla_estudi_id=UPE.modul_pla_estudi_id)
+		    WHERE N.matricula_id=IdMatricula;        
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+        OPEN cur;
+        read_loop: LOOP
+            FETCH cur INTO _ModulId;
+            IF done THEN
+                LEAVE read_loop;
+            END IF;
+		CALL CalculaNotaMitjanaModul(IdMatricula, _ModulId);
+        END LOOP;
+        CLOSE cur;
+    END;
+END //
+DELIMITER ;
+
+/*
+ * CalculaNotaMitjanaCurs
+ * Calcula les notes mitjanes de mòdul per a un curs. Ho fa matrícula per matrícula.
+ * @param integer CursId Identificador del curs.
+ */
+DELIMITER //
+CREATE PROCEDURE CalculaNotaMitjanaCurs(IN CursId INT)
+BEGIN
+    DECLARE _MatriculaId INT;
+    DECLARE done INT DEFAULT FALSE;
+
+    BEGIN
+        DECLARE cur CURSOR FOR
+			SELECT M.matricula_id FROM MATRICULA M
+			WHERE curs_id = CursId;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+        OPEN cur;
+        read_loop: LOOP
+            FETCH cur INTO _MatriculaId;
+            IF done THEN
+                LEAVE read_loop;
+            END IF;
+           CALL CalculaNotaMitjanaMatricula(_MatriculaId);
+        END LOOP;
+        CLOSE cur;
+    END;
+END //
+DELIMITER ;
+
+/*
  * CreaPlaEstudisModul
  * Crea el pla d'estudis per a un mòdul (copiant unitats actives).
  * @param integer ModulPlaEstudiId Identificador del mòdul.
